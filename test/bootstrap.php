b5db6745947ff453dc0a5508c065c32ccfc6322f
drak3 <drak3@live.de>
2012-01-14 22:02:12 +0100

    tidied up test dir

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register( function($class) {
     $path = str_replace("\\","/", $class);
-    $path = "/home/drak3/projects/TeamSpeak3-Framework/".$path.".php";
+    $path = __DIR__.'/../'.$path.".php";
     if(\file_exists($path))
         require_once $path;
 });
