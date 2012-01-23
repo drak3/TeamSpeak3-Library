19e4e9fe1a1ed90325274d92322affa2f9a5dff6
drak3 <drak3@live.de>
2012-01-23 12:28:16 +0100

    Added TransmissionStub for testing

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,9 +2,13 @@
 
 spl_autoload_register( function($class) {
     $path = str_replace("\\","/", $class);
-    $path = __DIR__.'/../'.$path.".php";
-    if(\file_exists($path))
-        require_once $path;
+    $path1 = __DIR__.'/../'.$path.".php";
+    $path2 = __DIR__.'/'.$path.'.php';
+    $path2 = str_replace("Test/", '', $path2);
+    if(\file_exists($path1))
+        require_once $path1;
+    if(\file_exists($path2))
+        require_once $path2;
 });
 
 
