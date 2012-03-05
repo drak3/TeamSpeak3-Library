84d2a54a049c7b0a80d05b163fe939e4148520d5
drak3 <drak3@live.de>
2012-03-05 12:27:27 +0100

    CommandFailedException now displays also failed_permid if present

--- src/devmx/Teamspeak3/Query/Exception/CommandFailedException.php
+++ src/devmx/Teamspeak3/Query/Exception/CommandFailedException.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
   This file is part of TeamSpeak3 Library.
 
@@ -20,26 +19,47 @@ namespace devmx\Teamspeak3\Query\Exception;
 use devmx\Teamspeak3\Query\CommandResponse;
 
 /**
- *
+ * The exception thrown when an command sent to the Query resulted in an error
+ * This is normaly detected by checking if the errorID is equal to 0 (= no error occured)
  * @author drak3
  */
 class CommandFailedException extends RuntimeException
 {
+    /**
+     * The response indicating the failure
+     * @var CommandResponse
+     */
     private $response;
     
+    /**
+     * @param CommandResponse $response the response caused by the failed command
+     */
     public function __construct(CommandResponse $response) {
         parent::__construct($this->buildMessage($response));
         $this->response = $response;
     }
     
+    /**
+     * Returns the response caused by the failed command
+     * This response contains (among other things) the errorID and the errorMessage
+     * @return CommandResponse
+     */
     public function getRespose() {
         return $this->response;
     }
     
+    /**
+     * Builds the Exceptionmessage, which does contain information about the commandname, the error id and message and, if present, the extra_message or failed_permid
+     * @param CommandResponse $response
+     * @return type 
+     */
     private function buildMessage(CommandResponse $response) {
         $message = sprintf('Command "%s" caused error with message "%s" and id %d.', $response->getCommand()->getName(), $response->getErrorMessage(), $response->getErrorID());
-        if($response->hasErrorValue( 'extra_message')) {
-            $message .= sprintf(' (Extra message: "%s")', $response->getErrorValue('extra_message'));
+        if($response->hasErrorValue('extra_message')) {
+            $message .= sprintf("\n".'Extra message: "%s"', $response->getErrorValue('extra_message'));
+        }
+        if($response->hasErrorValue('failed_permid')) {
+            $message .= sprintf("\n".'Not enough rights, failed permission ID: %d', $response->getErrorValue('failed_permid'));
         }
         return $message;
     }
