0c08a2f8d3b0cec805ac15b11b292b5a6d37b5e4
drak3 <drak3@live.de>
2012-03-08 20:19:19 +0100

    Changed TransmissionInterface (#18 and #19)

--- src/devmx/Transmission/TransmissionInterface.php
+++ src/devmx/Transmission/TransmissionInterface.php
@@ -26,8 +26,11 @@ interface TransmissionInterface
 
     /**
      * Establishs the transmission 
+     * @param float $timeout the timeout defines in seconds how long the transmission should try to establish itself -1 means that the method invoker has no special requirements to the timeout
+     *   the real timeout MUST be greater then $timout, it SHOULD take at least the first two decimal digits as microsecond timeout
+     * @throws \devmx\Transmission\Exception\EstablishingFailedException
      */
-    public function establish();
+    public function establish($timeout=-1);
     
     /**
      * Rertuns the hostname of the other end of the Transmission
@@ -50,29 +53,40 @@ interface TransmissionInterface
     
     /**
      * Sends the given data to the host
-     * @param string $data the data to send 
+     * @param string $data the data to send
+     * @param float $timeout the time in seconds after which the sending action is considered as broken if $timeout is set to -1 it means that the method invoker has no special requirements to the timeout
+     *   the real timeout MUST be greater or equal as $timeout, at least the first two decimal digits SHOULD be used to define the timeout
+     * @throws \devmx\Transmission\Exception\TimeoutException
      */
-    public function send($data);
+    public function send($data, $timeout=-1);
 
     /**
-     * Waits until a line end or data of the given length is received and returns the data (blocking)
-     * @param int $length if $length bytes are received, the data is returned
+     * Waits until a line end is received and returns the data (blocking)
+     * Caution: even if the timeout parameter is set to a value greater 0, 
+     *   the time this method takes could be indefinite long, because it doesn't return until a lineend is reached, or there is a gap of at least $timeout in the transmission
+     * @param float $timeout the time in seconds after which the receiving action is stopped when no new data was received if $timeout is set to -1 it means that the method invoker has no special requirements to the timeout
+     *        the real timeout MUST be greater or equal as $timeout, at least the first two decimal digits SHOULD be used to define the timeout
      * @return string the received data
+     * @throws \devmx\Transmission\Exception\TimeoutException
      */
-    public function receiveLine($length = 4096);
+    public function receiveLine($timeout=-1);
 
     /**
      * Returns all data currently on the stream (nonblocking)
      * @return string the data currently on the stream
      */
-    public function getAll();
+    public function checkForData();
 
     /**
      * Waits until given datalength is sent and returns it (blocking)
-     * @param $length the length of the data to wait for
-     * @return string
+     * Caution: the time this method takes could be longer than the given timeout
+     * @param int $length the length of the data to wait for
+     * @param float $timeout the time in seconds after which the receiving action is stopped when no new data was received if $timeout is set to -1 it means that the method invoker has no special requirements to the timeout
+     *        the real timeout MUST be greater or equal as $timeout, at least the first two decimal digits SHOULD be used to define the timeout
+     * @return string the received data
+     * @throws \devmx\Transmsission\Exception\TimeoutException
      */
-    public function receiveData($lenght);
+    public function receiveData($lenght, $timeout=-1);
 
     /**
      * Closes the transmission 
